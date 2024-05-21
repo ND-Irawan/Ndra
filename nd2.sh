b1d85e5bfadafc9304b69a5b4f65230374c6ed83
Ndra <80077633+IndraRN9@users.noreply.github.com>
2024-05-22 00:00:34 +0700

    Create nd2.sh

---
+++ nd2.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+DEFCONFIG="nd_defconfig"
+CLANGDIR="/workspace/build/clang18"
+
+#
+rm -rf out
+
+#
+mkdir -p out
+mkdir out/Ndra
+
+
+#
+export KBUILD_BUILD_USER=ꪀdra
+export KBUILD_BUILD_HOST=1507
+export PATH="$CLANGDIR/bin:$PATH"
+
+#
+make O=out ARCH=arm64 $DEFCONFIG
+
+#
+MAKE="./makeparallel"
+
+#
+START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+nd () {
+make -j$(nproc --all) O=out LLVM=1 \
+ARCH=arm64 \
+CC=clang \
+LD=ld.lld \
+AR=llvm-ar \
+AS=llvm-as \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+
+#RUN
+cp Ndra/sdm845-xiaomi-common.dtsi arch/arm64/boot/dts/qcom/
+cp Ndra/Gpu/sdm845-v2.dtsi arch/arm64/boot/dts/qcom/
+cp Ndra/Gpu/gpucc-sdm845.c drivers/clk/qcom/
+nd 2>&1 | tee -a compile.log
+if [ $? -ne 0 ]
+then
+    echo "Build failed"
+else
+    echo "Build succesful"
+    cp out/arch/arm64/boot/Image.gz-dtb out/Ndra/Image.gz-dtb
+fi
+
+END=$(date +"%s")
+DIFF=$(($END - $START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
