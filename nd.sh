0650bb25b25e461c46d90e7a0eb5d80baa26b756
Ndra <80077633+IndraRN9@users.noreply.github.com>
2024-05-27 17:50:49 +0700

    Add files via upload

---
+++ nd.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+DEFCONFIG="rvkernel/rvkernel_defconfig"
+CLANGDIR="/workspace/build/clang"
+
+#
+rm -rf out
+
+#
+mkdir -p out
+mkdir out/Ndra
+
+
+#
+export KBUILD_BUILD_USER=Son
+export KBUILD_BUILD_HOST=Ndra
+export PATH="$CLANGDIR/bin:$PATH"
+
+#
+make O=out ARCH=arm64 $DEFCONFIG
+
+#
+MAKE="./makeparallel"
+
+#
+START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+rve () {
+make -j$(nproc --all) O=out LLVM=1 \
+ARCH=arm64 \
+CC=clang \
+LD=ld.lld \
+AR=llvm-ar \
+AS=llvm-as \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+
+#SE Overclock
+cp RvKernel/SE/* arch/arm64/boot/dts/qcom/
+cp RvKernel/OC/sdm845-v2.dtsi arch/arm64/boot/dts/qcom/
+cp RvKernel/OC/gpucc-sdm845.c drivers/clk/qcom/
+rve | tee -a compile.log
+if [ $? -ne 0 ]
+then
+    echo "Build failed"
+else
+    echo "Build succesful"
+    cp out/arch/arm64/boot/Image.gz-dtb out/Ndra/Image.gz-dtb
+fi
+
+END=$(date +"%s")
+DIFF=$(($END - $START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
