c37b2b95e185bf831c905499accc9e43eb01d034
Ndra <80077633+ND-Irawan@users.noreply.github.com>
2024-08-01 22:28:27 +0700

    Add files via upload

---
+++ nd.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+DEFCONFIG="ndra_defconfig"
+CLANGDIR="/workspace/build/clang"
+
+#
+rm -rf out
+rm -rf compile.log
+
+#
+mkdir -p out
+mkdir out/ND
+
+
+#
+export KBUILD_BUILD_USER=Ndrα
+export KBUILD_BUILD_HOST=ND-Irαwαn
+export PATH="$CLANGDIR/bin:$PATH"
+
+#
+make O=out ARCH=arm64 $DEFCONFIG
+
+#
+MAKE="./makeparallel"
+
+#
+START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+nd () {
+make -j$(nproc --all) O=out LLVM=1 \
+ARCH=arm64 \
+CC=clang \
+LD=ld.lld \
+AR=llvm-ar \
+AS=llvm-as \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+
+#Run
+cp ND/SE/* arch/arm64/boot/dts/qcom/
+cp ND/GPU/sdm845-v2.dtsi arch/arm64/boot/dts/qcom/
+cp ND/GPU/gpucc-sdm845.c drivers/clk/qcom/
+cp ND/10.3.7/* firmware/
+nd 2>&1 | tee -a compile.log
+if [ $? -ne 0 ]
+then
+    echo "Build failed"
+else
+    echo "Build succesful"
+    cp out/arch/arm64/boot/Image.gz-dtb out/ND/Image.gz-dtb
+fi
+
+END=$(date +"%s")
+DIFF=$(($END - $START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
