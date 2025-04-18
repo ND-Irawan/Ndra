f91da433ab9b3ad7ab0cfb8e42bbb65633276427
ND-Irawan <indracoy01@gmail.com>
2025-04-18 20:30:36 +0700

    cpufreq: schedutil: Reduce frequencies slower
    The schedutil governor reduces frequencies too fast in some
    situations which cases undesirable performance drops to
    appear.
    
    To address that issue, make schedutil reduce the frequency slower by
    setting it to the average of the value chosen during the previous
    iteration of governor computations and the new one coming from its
    frequency selection formula.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -190,6 +190,9 @@ static bool sugov_update_next_freq(struct sugov_policy *sg_policy, u64 time,
 		sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 		return false;
 	}
+	
+	if (sg_policy->next_freq > next_freq)
+ 		next_freq = (sg_policy->next_freq + next_freq) >> 1;
 
 	sg_policy->next_freq = next_freq;
 	sg_policy->last_freq_update_time = time;
